7d55fc2c8c8d569fe43d87ecc8eade00ce3a292a
EvilMask <evilmask.dev@gmail.com>
2023-01-31 19:11:30 +0900

    docs: ConsoleEscapedParts.csの注釈を追加

--- Emuera/_Library/EvilMask/ConsoleEscapedParts.cs
+++ Emuera/_Library/EvilMask/ConsoleEscapedParts.cs
@@ -12,23 +12,38 @@ namespace EvilMask.Emuera
 {
 	internal sealed class ConsoleEscapedParts
 	{
-		static readonly DataTable dt = new DataTable();
-		static readonly Dictionary<Int64, AConsoleDisplayPart> parts = new Dictionary<long, AConsoleDisplayPart>();
+		const int ROW_LINE = 0, ROW_DEPTH = 1, ROW_TOP = 2, ROW_BOTTOM = 3, ROW_ID = 4, ROW_DIVTYPE = 5;
+		static readonly DataTable dt = new DataTable(); // divの情報を保存する場所
+		static readonly Dictionary<Int64, AConsoleDisplayPart> parts = new Dictionary<long, AConsoleDisplayPart>(); //　実際にdivデータを保存する場所
+		/// <summary>
+		/// 最近GetPartsInRangeを行ったか
+		/// </summary>
 		static bool getOnce = false;
+		/// <summary>
+		/// 前回GetPartsInRangeを行った時の状態
+		/// </summary>
 		static int lastTop, lastBottom, lastGeneration;
+		/// <summary>
+		/// 前回のGetPartsInRangeから変動があるか
+		/// </summary>
 		public static bool Changed { get; private set; }
+		/// <summary>
+		/// 前回GetPartsInRangeを行った時と状況が変わったらfalseを返す。
+		/// 重複計算防止用。
+		/// </summary>
 		public static bool TestedInRange(int top, int bottom, int gen)
 		{
 			return getOnce && top == lastTop && bottom == lastBottom && gen == lastGeneration;
 		}
 		static ConsoleEscapedParts()
 		{
-			dt.Columns.Add("line", typeof(int));
-			dt.Columns.Add("depth", typeof(int));
-			dt.Columns.Add("top", typeof(int));
-			dt.Columns.Add("bottom", typeof(int));
-			dt.Columns.Add("id", typeof(Int64));
-			dt.Columns.Add("div", typeof(sbyte));
+			// データベースを構築
+			dt.Columns.Add("line", typeof(int)); // div所属のline
+			dt.Columns.Add("depth", typeof(int)); // divの奥行き
+			dt.Columns.Add("top", typeof(int)); // divの一行目がこの行より下回る
+			dt.Columns.Add("bottom", typeof(int)); // divの一行目がこの行より上回る
+			dt.Columns.Add("id", typeof(Int64));　// ユニークID（保存時点のTimeStamp）
+			dt.Columns.Add("div", typeof(sbyte)); //　divの種類（display:absoluteかdisplay:relativeか）
 		}
 		public static void Clear()
 		{
@@ -41,37 +56,47 @@ namespace EvilMask.Emuera
 		{
 			var id = Utils.TimePoint();
 			var row = dt.NewRow();
-			row[0] = line;
-			row[1] = depth;
-			row[2] = top;
-			row[3] = bottom;
-			row[4] = id;
+			row[ROW_LINE] = line;
+			row[ROW_DEPTH] = depth;
+			row[ROW_TOP] = top;
+			row[ROW_BOTTOM] = bottom;
+			row[ROW_ID] = id;
 			if (part is ConsoleDivPart div)
-				row[5] = (!div.IsRelative ? 2 : 0) | 1;
+				row[ROW_DIVTYPE] = (!div.IsRelative ? 2 : 0) | 1;　// bit1 : divであるなら1　bit2 : divがdisplay:absoluteなら1
 			else
-				row[5] = 0;
+				row[ROW_DIVTYPE] = 0;
 			dt.Rows.Add(row);
 			parts.Add(id, part);
 			Changed = true;
 		}
+		/// <summary>
+		/// 所在行のlineNoがlineの行とその以降の行のdivデータを全部削除
+		/// </summary>
 		public static void Remove(int line)
 		{
-			foreach (var row in DataTableExtensions.AsEnumerable(dt).Where(r => (int)r[0] >= line).ToArray())
+			foreach (var row in DataTableExtensions.AsEnumerable(dt).Where(r => (int)r[ROW_LINE] >= line).ToArray())
 			{
-				parts.Remove((Int64)row[4]);
+				parts.Remove((Int64)row[ROW_ID]);
 				dt.Rows.Remove(row);
 				Changed = true;
 			}
 		}
+		/// <summary>
+		/// 所在行のlineNoがlineの行のdivデータを削除
+		/// </summary>
 		public static void RemoveAt(int line)
 		{
-			foreach (var row in DataTableExtensions.AsEnumerable(dt).Where(r => (int)r[0] == line).ToArray())
+			foreach (var row in DataTableExtensions.AsEnumerable(dt).Where(r => (int)r[ROW_LINE] == line).ToArray())
 			{
-				parts.Remove((Int64)row[4]);
+				parts.Remove((Int64)row[ROW_ID]);
 				dt.Rows.Remove(row);
 				Changed = true;
 			}
 		}
+		/// <summary>
+		/// top行からbottom行までの範囲内表示内容があったdivをrmapで保存。
+		/// genは今のbuttonGeneration，重複計算防止用。
+		/// </summary>
 		public static void GetPartsInRange(int top, int bottom, int gen, Dictionary<int, List<AConsoleDisplayPart>> rmap)
 		{
 			if (GlobalStatic.Console?.GetLineNo > Config.MaxLog)
@@ -82,18 +107,22 @@ namespace EvilMask.Emuera
 			}
 			if (rmap == null) return;
 			rmap.Clear();
+			// すべてのdisplay:absoluteのdiv
+			// とtop行からbottom行までの範囲内表示内容があったdiv
+			// と所在行がtop行からbottom行までの範囲内のdiv
+			// に対し
 			foreach (var row in DataTableExtensions.AsEnumerable(dt)
-				.Where(r => ((sbyte)r[5] & 2) != 0 || ((int)r[2] <= bottom + 1 && (int)r[3] >= top && r[0] is int line
-				&& ((sbyte)r[5] != 0 || top > line || line > bottom + 1))))
+				.Where(r => ((sbyte)r[ROW_DIVTYPE] & 2) != 0 || ((int)r[ROW_TOP] <= bottom + 1 && (int)r[ROW_BOTTOM] >= top && r[ROW_LINE] is int line
+				&& ((sbyte)r[ROW_DIVTYPE] != 0 || top > line || line > bottom + 1))))
 			{
 				List<AConsoleDisplayPart> list = null;
-				rmap.TryGetValue((int)row[1], out list);
+				rmap.TryGetValue((int)row[ROW_DEPTH], out list);
 				if (list == null)
 				{
 					list = new List<AConsoleDisplayPart>();
-					rmap.Add((int)row[1], list);
+					rmap.Add((int)row[ROW_DEPTH], list);
 				}
-				list.Add(parts[(Int64)row[4]]);
+				list.Add(parts[(Int64)row[ROW_ID]]);
 			}
 			getOnce = true;
 			lastTop = top; lastBottom = bottom; lastGeneration = gen;
