5cca9bb00bbc2058ec388bab0c3ce6f123f6fe6a
Enter <deletedenter@gmail.com>
2024-05-01 15:16:19 +0900

    merge:VVII's "ProgramのNullable化" https://gitlab.com/VVIIlet/emuera/-/commit/d935064496bb7b8c8aae02158fd75b9ab5e5f69d

--- Emuera/Program.cs
+++ Emuera/Program.cs
@@ -309,20 +309,20 @@ static class Program
 	/// <summary>
 	/// 実行ファイルのディレクトリ。最後に\を付けたstring
 	/// </summary>
-	public static string ExeDir { get; private set; }
-	public static string WorkingDir { get; private set; }
+	public static string? ExeDir { get; private set; }
+	public static string? WorkingDir { get; private set; }
 	#endregion
-	public static string CsvDir { get; private set; }
-	public static string ErbDir { get; private set; }
-	public static string DebugDir { get; private set; }
-	public static string DatDir { get; private set; }
-	public static string ContentDir { get; private set; }
-	public static string ExeName { get; private set; }
+	public static string? CsvDir { get; private set; }
+	public static string? ErbDir { get; private set; }
+	public static string? DebugDir { get; private set; }
+	public static string? DatDir { get; private set; }
+	public static string? ContentDir { get; private set; }
+	public static string? ExeName { get; private set; }
 	#region EE_PLAYSOUND系
-	public static string MusicDir { get; private set; }
+	//public static string? MusicDir { get; private set; }
 	#endregion
 	#region EE_フォントファイル対応
-	public static string FontDir { get; private set; }
+	public static string? FontDir { get; private set; }
 	#endregion
 
 
