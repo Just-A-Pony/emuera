ddfd8502f05c06807a652ffcb9edb27a0ac99bee
Enter <deletedenter@gmail.com>
2024-06-04 20:10:21 +0900

    merge:VVII's "不要なToUpperを除去" https://gitlab.com/VVIIlet/emuera/-/commit/0e4e09738078a9fc6099dea1a87f1c4d96b11e4d

--- Emuera/UI/Game/Graphics/AppContents.cs
+++ Emuera/UI/Game/Graphics/AppContents.cs
@@ -128,7 +128,7 @@ static class AppContents
 					{
 						//アニメスプライト宣言。nullでないとき、フレーム追加モード
 						SpriteAnime currentAnime = null;
-						string directory = Path.GetDirectoryName(path).ToUpper() + "\\";
+						string directory = Path.GetDirectoryName(path) + "\\";
 						string filename = Path.GetFileName(path);
 						string[] lines = File.ReadAllLines(path, EncodingHandler.DetectEncoding(path));
 						int lineNo = 0;
@@ -214,12 +214,12 @@ static class AppContents
 	{
 		if (tokens.Length < 2)
 			return null;
-		string name = tokens[0].Trim().ToUpper();//
-		string arg2 = tokens[1].ToUpper();//画像ファイル名
+		string name = tokens[0].Trim();//
+		string arg2 = tokens[1];//画像ファイル名
 		if (name.Length == 0 || arg2.Length == 0)
 			return null;
 		//アニメーションスプライト宣言
-		if (arg2 == "ANIME")
+		if (arg2.Equals("ANIME", StringComparison.OrdinalIgnoreCase))
 		{
 			if (tokens.Length < 4)
 			{
