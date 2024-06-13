04f43a6f3adc804e05fd9c573dbd0abdc85dfeeb
Enter <deletedenter@gmail.com>
2024-06-13 16:19:03 +0900

    merge:VVII's "エラー追加処理の微修正" https://gitlab.com/VVIIlet/emuera/-/commit/4269046ecc865e6d5858bd3f17d1cb23a9b4bc4e

--- Emuera/Runtime/Script/Data/ParserMediator.cs
+++ Emuera/Runtime/Script/Data/ParserMediator.cs
@@ -77,6 +77,7 @@ internal partial class ParserMediator
 	}
 	#endregion
 
+	static object warningListLock = new();
 
 	public static void Warn(string str, ScriptPosition? pos, int level)
 	{
@@ -88,7 +89,12 @@ internal partial class ParserMediator
 		if (level < Config.DisplayWarningLevel && !Program.AnalysisMode)
 			return;
 		if (console != null && !console.RunERBFromMemory)
-			warningList.Add(new ParserWarning(str, pos, level, stack));
+		{
+			lock (warningListLock)
+			{
+				warningList.Add(new ParserWarning(str, pos, level, stack));
+			}
+		}
 	}
 
 	/// <summary>
